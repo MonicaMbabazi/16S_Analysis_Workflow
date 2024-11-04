09cad3391fbaa792323b6fb3b296353b713d2e2f
Monica Mbabazi <58783705+MonicaMbabazi@users.noreply.github.com>
2024-11-04 20:19:29 +0300

    dada2

--- Scripts/dada2_taxonomy.sh
+++ Scripts/dada2_taxonomy.sh
@@ -19,13 +19,16 @@
 module load anaconda/2024.02
 conda activate qiime2-amplicon-2024.5
 
+## Define the Results/Output directory
+RESULTS_DIR="absolute-path-output-dir"
+
 # 1. Assume you run your entire dataset as a single dataset. Meaning you imported one manifest file and you have one demux.qza file
 ## Run DADA2 using the code below
 qiime dada2 denoise-paired \
-  --i-demultiplexed-seqs /absolute-path-output-dir/demux.qza \
-  --o-table /absolute-path-output-dir/table-dada.qza \
-  --o-representative-sequences /absolute-path-output-dir/rep-seqs-dada.qza \
-  --o-denoising-stats /absolute-path-output-dir/stats-dada.qza \
+  --i-demultiplexed-seqs ${RESULTS_DIR}/demux.qza \
+  --o-table ${RESULTS_DIR}/table-dada.qza \
+  --o-representative-sequences ${RESULTS_DIR}/rep-seqs-dada.qza \
+  --o-denoising-stats ${RESULTS_DIR}/stats-dada.qza \
   --p-trim-left-f 5 \
   --p-trim-left-r 5 \
   --p-trunc-len-r 220 \
@@ -33,59 +36,85 @@ qiime dada2 denoise-paired \
   --p-n-threads 6
 
 # 2. If you ran your data in batches. Assume you have 15 batches. So, we have to run dada2 on each batch and then after merge dada2 outputs
+## Run DADA2 on 15 batches using the code below
+qiime dada2 denoise-paired \
+  --i-demultiplexed-seqs ${RESULTS_DIR}/demux_"${BatchN}".qza \
+  --o-table ${RESULTS_DIR}/table-dada_"${BatchN}".qza \
+  --o-representative-sequences ${RESULTS_DIR}/rep-seqs-dada_"${BatchN}".qza \
+  --o-denoising-stats ${RESULTS_DIR}/stats-dada_"${BatchN}".qza \
+  --p-trim-left-f 5 \
+  --p-trim-left-r 5 \
+  --p-trunc-len-r 220 \
+  --p-trunc-len-f 220 \
+  --p-n-threads 6
 
-## Merging dada2 table
-#DADA2_DIR="/exports/eddie/scratch/s2000755/Meta-analysis/Results"
+## Merging dada2 output files: 1. table files,  2.representative sequence files 
 
-# Collect all table files using a wildcard for SGE_TASK_ID
-#qiime feature-table merge \
- # --i-tables $(ls ${DADA2_DIR}/table-dada_*.qza | tr '\n' ' ') \
- # --o-merged-table ${DADA2_DIR}/merged-table.qza
+# Collect all table files using a wildcard
+qiime feature-table merge \
+  --i-tables $(ls ${RESULTS_DIR}/table-dada_*.qza | tr '\n' ' ') \
+  --o-merged-table ${RESULTS_DIR}/table-dada.qza
 
-# Collect all rep-seqs files using a wildcard for SGE_TASK_ID
-#qiime feature-table merge-seqs \
- # --i-data $(ls ${DADA2_DIR}/rep-seqs-dada_*.qza | tr '\n' ' ') \
- # --o-merged-data ${DADA2_DIR}/merged-rep-seqs.qza
+# Collect all rep-seqs files using a wildcard 
+qiime feature-table merge-seqs \
+  --i-data $(ls ${RESULTS_DIR}/rep-seqs-dada_*.qza | tr '\n' ' ') \
+  --o-merged-data ${RESULTS_DIR}/rep-seqs-dada.qza
 
-######################################### AFTER MERGING #######################################################
+########################################################### End of DADA2 step #################################################################
+
+#  Note: After DADA2 step you follow the same steps below, whether the dataset was run as one batch or in many batches.
 
 ######################## Diversity analysis of the merged data #########################
+# This step takes in  dada2 output 
 qiime phylogeny align-to-tree-mafft-fasttree \
-  --i-sequences /exports/eddie/scratch/s2000755/Meta-analysis/Results/merged-rep-seqs.qza \
-  --o-alignment /exports/eddie/scratch/s2000755/Meta-analysis/Results/aligned-rep-seqs_merged.qza \
-  --o-masked-alignment /exports/eddie/scratch/s2000755/Meta-analysis/Results/masked-aligned-rep-seqs_merged.qza \
-  --o-tree /exports/eddie/scratch/s2000755/Meta-analysis/Results/unrooted-tree_merged.qza \
-  --o-rooted-tree /exports/eddie/scratch/s2000755/Meta-analysis/Results/rooted-tree_merged.qza
+  --i-sequences ${RESULTS_DIR}/rep-seqs-dada.qza \
+  --o-alignment ${RESULTS_DIR}/aligned-rep-seqs.qza \
+  --o-masked-alignment ${RESULTS_DIR}/masked-aligned-rep-seqs.qza \
+  --o-tree ${RESULTS_DIR}/unrooted-tree.qza \
+  --o-rooted-tree ${RESULTS_DIR}/rooted-tree.qza
 
 ###### Alpha and Beta Diversity #####################################
+# The input files are: rooted-tree.qza and table-dada.qza
 qiime diversity core-metrics-phylogenetic \
-  --i-phylogeny /exports/eddie/scratch/s2000755/Meta-analysis/Results/rooted-tree_merged.qza \
-  --i-table /exports/eddie/scratch/s2000755/Meta-analysis/Results/merged-table.qza \
-  --p-sampling-depth 5000 \
-  --m-metadata-file /exports/eddie/scratch/s2000755/Meta-analysis/combined_metadata.tsv \
-  --output-dir /exports/eddie/scratch/s2000755/Meta-analysis/Results/core-metrics-results_merged
+  --i-phylogeny ${RESULTS_DIR}/rooted-tree.qza \
+  --i-table ${RESULTS_DIR}/table-dada.qza \
+  --p-sampling-depth 5000 \ # set the sampling depth depending on read depth of your dataset
+  --m-metadata-file /absolute-path/metadata.tsv \
+  --output-dir ${RESULTS_DIR}/core-metrics-results
 
 #### Alpha rarefaction
 qiime diversity alpha-rarefaction \
-  --i-table /exports/eddie/scratch/s2000755/Meta-analysis/Results/merged-table.qza \
-  --i-phylogeny /exports/eddie/scratch/s2000755/Meta-analysis/Results/rooted-tree_merged.qza \
+  --i-table ${RESULTS_DIR}/table-dada.qza \
+  --i-phylogeny ${RESULTS_DIR}/rooted-tree.qza \
   --p-max-depth 10000 \
-  --m-metadata-file /exports/eddie/scratch/s2000755/Meta-analysis/combined_metadata.tsv \
-  --o-visualization /exports/eddie/scratch/s2000755/Meta-analysis/Results/alpha-rarefaction_merged.qzv
+  --m-metadata-file /absolute-path/metadata.tsv \
+  --o-visualization ${RESULTS_DIR}/alpha-rarefaction.qzv
 
 ##### Taxonomic assignment
 qiime feature-classifier classify-sklearn \
-  --i-classifier /exports/eddie/scratch/s2000755/Meta-analysis/silva-138.1-full-nr99-classifier.qza \
-  --i-reads /exports/eddie/scratch/s2000755/Meta-analysis/Results/merged-rep-seqs.qza \
-  --o-classification /exports/eddie/scratch/s2000755/Meta-analysis/Results/taxonomy_merged.qza
+  --i-classifier /absolute-path/classifier.qza \
+  --i-reads ${RESULTS_DIR}/rep-seqs-dada.qza \
+  --o-classification ${RESULTS_DIR}/taxonomy.qza
 
+### Visualize the taxonomy output
 qiime metadata tabulate \
-  --m-input-file /exports/eddie/scratch/s2000755/Meta-analysis/Results/taxonomy_merged.qza \
-  --o-visualization /exports/eddie/scratch/s2000755/Meta-analysis/Results/taxonomy_merged.qzv
+  --m-input-file ${RESULTS_DIR}/taxonomy.qza \
+  --o-visualization ${RESULTS_DIR}/taxonomy.qzv
 
 #### Taxa barplot
 qiime taxa barplot \
-  --i-table /exports/eddie/scratch/s2000755/Meta-analysis/Results/merged-table.qza \
-  --i-taxonomy /exports/eddie/scratch/s2000755/Meta-analysis/Results/taxonomy_merged.qza \
-  --m-metadata-file /exports/eddie/scratch/s2000755/Meta-analysis/combined_metadata.tsv \
-  --o-visualization /exports/eddie/scratch/s2000755/Meta-analysis/Results/taxa-bar-plots_merged.qzv
+  --i-table ${RESULTS_DIR}/table-dada.qza \
+  --i-taxonomy ${RESULTS_DIR}/taxonomy.qza \
+  --m-metadata-file /absolute-path/metadata.tsv \
+  --o-visualization ${RESULTS_DIR}/taxa-bar-plots.qzv
+
+################################################################################# End  ##############################################################################
+
+### Important files required to generate phyloseq object
+# 1. table-dada.qza
+# 2. rooted-tree.qza
+# 3. taxonomy.qza
+# 4. metadata.tsv
+
+#####  To be imported in R for downstream analysis
+#####################################################################################################################################################################
